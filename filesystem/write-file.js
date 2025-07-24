9759d96f380b61cfbf67d38745d017bdf8e5124c
Nadeem-pc <nadeempc777@gmail.com>
2025-07-24 18:39:59 +0530

    feat: Add file creation examples using fs methods and Promises

---
+++ filesystem/write-file.js
@@ -0,0 +1,42 @@
+const fs = require('fs')
+
+// For creating a new file
+fs.writeFile('sample.txt', 'Content will be given here', (err) => {
+    if(err) {
+        console.log(err)
+    }
+    console.log('File created successfully...')
+})
+
+
+// Creating file using writeFileSync
+try {
+    fs.writeFileSync('async.txt', 'Hi my name is nadeem')
+    console.log('File created successfully')
+} catch (error) {
+    console.log(error)
+}
+
+
+// Write a file when a Promise resolves
+function writeFile(content) {
+    return new Promise((resolve, reject) => {
+        const taskCompleted = true
+        if(taskCompleted){
+            resolve(content)
+        }else{
+            reject('Task Failed !')
+        }
+    })
+}
+
+writeFile("Have a nice day")
+.then((content) => {
+    return fs.writeFile("promiseCreatedFile.txt", content)
+})
+.then(() => {
+    console.log('File created successfully...')
+})
+.catch((err) => {
+    console.error("An error occured", err)
+})
