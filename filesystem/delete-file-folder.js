3a119fbf1155359130df7ff0635402126416b513
Nadeem-pc <nadeempc777@gmail.com>
2025-07-26 10:15:37 +0530

    feat: Add folder deletion code with fs module

--- filesystem/delete-file-folder.js
+++ filesystem/delete-file-folder.js
@@ -1,5 +1,12 @@
 // Creating a new folder 
-fs.mkdir('newFolder', (err) => {
+fs.mkdir('folderName', (err) => {
     if (err) throw err;
     console.log('folder created successfully')
+})
+
+
+// Deleting a folder
+fs.rmdir('folderName', (err) => {
+    if (err) throw err;
+    console.log('folder deleted successfully')
 })
