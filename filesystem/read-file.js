6d5e4da32fbaa0a2f933df88704333d0956cbde2
Nadeem-pc <nadeempc777@gmail.com>
2025-07-27 14:12:48 +0530

    feat: add server to read and display file content

--- filesystem/read-file.js
+++ filesystem/read-file.js
@@ -16,3 +16,22 @@ try {
 } catch (error) {
     console.log(error)
 }
+
+
+// Read a file and display on browser
+const express = require('express')
+const app = express()
+const fs = require('fs')
+
+let fileContent
+fs.readFile('sample.txt', 'utf-8', (err, data) => {
+    fileContent = data
+})
+
+app.get('/', (req, res) => {
+    res.send(fileContent)
+})
+
+app.listen(4000, () => {
+    console.log('Server is running on port 4000...')
+})
