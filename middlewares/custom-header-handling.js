0a5a2d8986834c848bb1192ae79881f1658fb733
Nadeem-pc <nadeempc777@gmail.com>
2025-07-29 10:00:52 +0530

    feat: add middleware to block GET requests

--- middlewares/custom-header-handling.js
+++ middlewares/custom-header-handling.js
@@ -4,4 +4,13 @@ app.use((req, res, next) => {
         res.setHeader('X-Custom-Header', 'Header added for get request')
     }
     next()
-})  
+})
+
+// middleware to block get request with custom header.
+app.use((req, res, next) => {
+    if(req.method === 'GET'){
+        res.setHeader('X-Custom-Header', 'Get request are blocked')
+        return res.status(403).send('Get requests are not allowed')
+    }
+    next()
+})
