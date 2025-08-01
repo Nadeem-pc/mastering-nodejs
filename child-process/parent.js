1204ab0a4b063c8d25fdb69a96ea0741589373b3
Nadeem-pc <nadeempc777@gmail.com>
2025-08-01 09:27:23 +0530

    feat: add simple child process communication example

---
+++ child-process/parent.js
@@ -0,0 +1,8 @@
+const { fork } = require('child_process')
+const child = fork('./child.js')
+
+child.on('message', (msg) => {
+    console.log('Message from child: ', msg)
+})
+
+child.send('Hello Child')
